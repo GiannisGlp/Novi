031466f608ce4940061dd8ae63e63751dffd6f00
GiannisGlp <giannis4mi@outlook.com>
2026-08-19 15:39:27 +0100

    Export Mac neural model interfaces

---
+++ mac_brain/models/__init__.py
@@ -0,0 +1,9 @@
+from .local_detector import LocalNeuralObjectDetector
+from .object_detection import Detection, DeterministicObjectDetector, ObjectDetector
+
+__all__ = [
+    "Detection",
+    "DeterministicObjectDetector",
+    "LocalNeuralObjectDetector",
+    "ObjectDetector",
+]
